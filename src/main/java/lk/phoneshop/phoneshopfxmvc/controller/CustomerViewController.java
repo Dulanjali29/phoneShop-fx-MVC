e9218b88431fb6bb35e1ec9ced4c3825b5c1b082
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-01-02 20:37:13 +0530

    finish ui

--- src/main/java/lk/phoneshop/phoneshopfxmvc/controller/CustomerViewController.java
+++ src/main/java/lk/phoneshop/phoneshopfxmvc/controller/CustomerViewController.java
@@ -1,4 +1,49 @@
 package lk.phoneshop.phoneshopfxmvc.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerViewController {
+    @FXML
+    private TableView<?> tblcustomer;
+
+    @FXML
+    private TextField txtadd;
+
+    @FXML
+    private TextField txtcontact;
+
+    @FXML
+    private TextField txtcustid;
+
+    @FXML
+    private TextField txtnic;
+
+    @FXML
+    private TextField txtnm;
+
+    @FXML
+    private TextField txtuid;
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search(ActionEvent event) {
+
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+
+    }
 }
--- src/main/java/lk/phoneshop/phoneshopfxmvc/controller/DashboardViewController.java
+++ src/main/java/lk/phoneshop/phoneshopfxmvc/controller/DashboardViewController.java
@@ -1,4 +1,22 @@
 package lk.phoneshop.phoneshopfxmvc.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DashboardViewController {
+
+    @FXML
+    void customer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void order(ActionEvent event) {
+
+    }
+
+    @FXML
+    void phone(ActionEvent event) {
+
+    }
 }
